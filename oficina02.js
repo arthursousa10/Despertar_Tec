34837a8ef7df8244b2c86ccde5dc5d6911795ece
Arthur <105012211+arthursousa10@users.noreply.github.com>
2023-03-10 15:43:27 -0300

    Add files via upload

---
+++ oficina02.js
@@ -0,0 +1,17 @@
+let TemDinehiro = false;
+//
+let temSol = true; 
+
+if (TemDinehiro) {
+    if (temSol) {
+        console.log("Guarujá");
+    } else {
+        console.log("Restaurante");
+    }
+} else {
+    if (temSol) {
+        console.log("Ibiapuera");
+    } else {
+        console.log("Aula");
+    }
+}
